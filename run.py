2a00d3b3cfdfae6ee02209aa03113b159e8e2c5a
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-15 12:23:10 +0100

    restart, google fonts ref not working but everything else

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+#! /usr/local/bin/python3
+
+# Ciarans template on flask
+
 import os
 from flask import Flask, render_template
 
@@ -8,6 +11,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == '__main__':
