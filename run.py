29496a19f5376a898e346e18a1cf639c5cc64231
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-17 18:56:05 +0100

    2

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
+#! /usr/local/bin/python
+# Ciarans template on flask
+
+import json
 import os
-from flask import Flask
+from flask import Flask, render_template, request, flash
+from pathlib import Path
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "Hello Worldz"
 
 
 if __name__ == '__main__':
