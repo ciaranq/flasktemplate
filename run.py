eaae92abf96d895fcd62ca70f5d837592c8a05d4
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-17 11:41:06 +0100

    getting ready to deploy

--- run.py
+++ run.py
@@ -13,34 +13,38 @@ mypath = Path().absolute()
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-
+# Passing in following variables to the html page
+# page_title, page_heading, cta.  - these are used in the hero imaage.
+#
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html", page_title="Heh Welcome Y'all", page_heading="We are team of awesome designers making websites with Full Stack stuff", list_stuff_wedo=["Bootstrap", "Django", "Flask", "Python", "Javascript"])
+    return render_template("index.html", page_title="Heh Welcome Y'all", page_heading="We are team of awesome designers making websites with Full Stack stuff", cta="Get Started", list_stuff_wedo=["Bootstrap", "Django", "Flask", "Python", "Javascript"])
 
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-# seperate contact page for this , taken from bottom of page
+# The contact page take a post and flashes to a screen a thank yu message.
+# Plan to add this json post data to a static or DB file.
+#
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         print(request.form["name"])
         flash("Thats cool {},we got your note!" .format(request.form["name"]))
-        # flash("Thanks {}, we have received your message".format(request.form["name"]))
-    return render_template("contact.html")
+    return render_template("contact.html", heroimage="static/img/hero-bg.jpg", page_title="Touch base with us", page_heading="Book a coffee chat withn us, send a message or request a quote", cta="Let's do this")
 
-# must use the url_for('magento') tag to refere to these
+# Created a seperate page for the url_for('magento').
+#
 @app.route('/magento')
 def magento():
     data = []
     with open("data/static.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("magento.html", page_title="Magento data via API's", staticMage=data)
+    return render_template("magento.html", page_title="Magento data via API's", page_heading="Find out about our Magent integration options", cta="Run the APi now", staticMage=data)
 
-
+# this decorator is for pages called under magento
 @app.route('/magento/<member_name>', methods=["GET", "POST"])
 def about_member(member_name):
     member = {}
@@ -49,7 +53,7 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-   # return "<h1>"+member["name"]+"</h1>"
+    # return "<h1>"+member["name"]+"</h1>"
     return render_template("member.html", member=member)
 
 
